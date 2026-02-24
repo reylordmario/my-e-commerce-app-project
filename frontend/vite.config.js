ba465c553970c1f859d22971091ee34aea9f00ef
ReylordMarioEchala <echalareylordmario@gmail.com>
2026-02-24 23:50:11 +0800

    Configure GitHub Pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  // This must match your repository name exactly
+  base: "https://github.com/reylordmario/my-e-commerce-app-project.git", 
 })
